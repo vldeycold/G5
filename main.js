28de39b5cdddfb862c0a673a7ac9dd439f8c6b43
vldeycold <tubum02@gmail.com>
2026-01-05 19:05:51 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://whatsapp.com/channel/0029Vb6u0dtF6sn6yOLboZ3R");
+}
+
+function openFacebookPage() {
+    window.open("https://www.facebook.com/share/1AHBZiuybF/");
+}
+
+const targetURL = "https://otieu.com/4/10181511";
+let opened = false;
+
+const videoOverlay = document.getElementById("videoOverlay");
+
+if (videoOverlay) {
+    videoOverlay.addEventListener("click", function () {
+        if (!opened) {
+            opened = true;
+            window.open(targetURL, "_blank"); // BUKA TAB BARU
+        }
+        this.remove(); // setelah klik pertama, video bisa dikontrol normal
+    });
+}
+
+
+
+
